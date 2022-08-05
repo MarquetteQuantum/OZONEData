01e7556414b9b4f9f37cde7c8e0483ef2424a1cc
IgorGayday <gaidai92@gmail.com>
2022-08-05 16:24:35 -0500

    Updates my_plot, adds krec convergence plots vs emin and emax

--- general/plots/my_plot.m
+++ general/plots/my_plot.m
@@ -1,15 +1,17 @@
-function figure = my_plot(xs, ys, optional)
+function figure = my_plot(xs, ys, x_label, y_label, optional)
 % Plots data in plots_data using several predefined options
   arguments
     xs (1, :) double
     ys (1, :) double
+    x_label (1, 1) string = ""
+    y_label (1, 1) string = ""
     optional.new_plot (1, 1) logical = true
     optional.color (1, 1) string = "b"
     optional.marker (1, 1) string = "."
     optional.line_width (1, 1) double = 2
     optional.marker_size (1, 1) double = 20
-    optional.xlabel (1, 1) string = ""
-    optional.ylabel (1, 1) string = ""
+    optional.xdir (1, 1) string = "normal"
+    optional.yscale (1, 1) string = "linear"
   end
 
   if optional.new_plot
@@ -19,6 +21,15 @@ function figure = my_plot(xs, ys, optional)
   figure = plot(xs, ys, 'Color', optional.color, 'Marker', optional.marker, 'LineWidth', optional.line_width, ...
     'MarkerSize', optional.marker_size);
 
-  xlabel(optional.xlabel);
-  ylabel(optional.ylabel);
+  xlabel(x_label);
+  ylabel(y_label);
+  set(gca, "xdir", optional.xdir);
+  set(gca, "yscale", optional.yscale);
+
+%   zoom_out_x();
+%   if optional.yscale == "linear"
+%     zoom_out_y();
+%   else
+%     zoom_out_log_y();
+%   end
 end
---
+++ kinetics/time_dependent/plots/plot_krec_eig_vs_emax.m
@@ -0,0 +1,32 @@
+function plot_krec_eig_vs_emax()
+% Plots krec vs lower energy bound (convergence)
+  resonances = getvar('resonances');
+  j_per_cm_1 = getvar('j_per_cm_1');
+  m_per_a0 = getvar('m_per_a0');
+  
+  o3_molecule = '666';
+  Js = 24;
+  Ks = 2;
+  syms = 0;
+  temp_k = 298;
+  M_per_m3 = 6.44e24;
+  dE_j = [-43.13, nan] * j_per_cm_1;
+  dE_j(2) = get_dE_up(dE_j(1), temp_k);
+  sigma0_m2 = 250 * m_per_a0^2;
+  optional = map_create();
+  optional('k_dependent_threshold') = 0;
+  emaxs = 200;
+%   emaxs = 100:100:1000;
+
+  key = get_key(o3_molecule, Js, Ks, syms);
+  states = resonances(key);
+%   states = states(states{:, 'energy'} > -3000 * j_per_cm_1, :);
+  states = assign_extra_properties(o3_molecule, states);
+  krecs_m6_per_s = zeros(size(emaxs));
+  for i = 1:length(emaxs)
+    next_states = states(states{:, 'energy'} < emaxs(i) * j_per_cm_1, :);
+    krecs_m6_per_s(i) = find_krec_eig(o3_molecule, temp_k, sigma0_m2, next_states, dE_j, M_per_m3, optional);
+  end
+  
+  my_plot(emaxs, krecs_m6_per_s, "E_{max}, cm^{-1}", "k_{rec}, m^6/s", yscale = "log");
+end
