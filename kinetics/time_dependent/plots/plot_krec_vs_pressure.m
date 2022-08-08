85a53b0c14ac46e3e2316079cb7591e9806d7b65
Adil Yermek <adilermek1@yandex.ru>
2022-08-08 11:29:02 -0500

    Update plot_krec_vs_pressure.m

--- kinetics/time_dependent/plots/plot_krec_vs_pressure.m
+++ kinetics/time_dependent/plots/plot_krec_vs_pressure.m
@@ -2,10 +2,9 @@ function plot_krec_vs_pressure()
   resonances = getvar('resonances');
   j_per_cm_1 = getvar('j_per_cm_1');
   m_per_a0 = getvar('m_per_a0');
-  k0 = 2.42e-16;
   
   o3_molecule = '666';
-  Js = 0:32;
+  Js = [0:32, 36:4:64];
   Ks = 0:20;
   syms = 0:1;
   temp_k = 298;
@@ -15,6 +14,8 @@ function plot_krec_vs_pressure()
   sigma0_m2 = 1500 * m_per_a0^2;
   optional = map_create();
   optional('k_dependent_threshold') = 0;
+  k0_m3_per_s = get_k0_2(o3_molecule, temp_k, sigma0_m2);
+%   k0_1 = 14.52e-16;
   
   tic
   krecs_m6_per_s = zeros(size(Ms_per_m3));
@@ -24,17 +25,20 @@ function plot_krec_vs_pressure()
       J = Js(J_ind);
       for K_ind = 1:length(Ks)
         K = Ks(K_ind);
-        if K > J
+
+        if K > J || J > 32 && mod(K, 2) ~= 0
           continue
         end
 
         for sym_ind = 1:length(syms)
           sym = syms(sym_ind);
+          
           key = get_key(o3_molecule, J, K, sym);
           states = resonances(key);
           states = states(states{:, 'energy'} > -3000 * j_per_cm_1, :);
           states = states(states{:, 'energy'} < 300 * j_per_cm_1, :);
-          states = states(states{:, 'gamma_total'} < 1e-10 * k0 * M_per_m3 * j_per_cm_1, :);
+          states = states(states{:, 'gamma_total'} < 1e-10 * k0_m3_per_s * M_per_m3 * j_per_cm_1, :);
+%           states = states(states{:, 'gamma_total'} < 0.1 * j_per_cm_1, :);
           states = assign_extra_properties(o3_molecule, states);
           
           dtau = get_dtau_2(J, K, sym);
@@ -49,7 +53,7 @@ function plot_krec_vs_pressure()
   experimental = get_krec_vs_pressure_experimental_666();
   krec_exp_m6_per_s = experimental(:, 2) * 1e-12;
   pressure = m_conc_to_bar(Ms_per_m3, temp_k);
-%   create_x_log_plot();
+  create_x_log_plot();
   plot(pressure, krecs_m6_per_s, 'b.-', 'MarkerSize', 10);
 %   yyaxis right
   plot(experimental(:, 1), krec_exp_m6_per_s, 'k.', 'MarkerSize', 20);
