24ce638b91b6d6653cbc077cb1ec03fdbae5fc88
IgorGayday <gaidai92@gmail.com>
2022-10-05 15:29:13 -0500

    Sends computed data to the host immediately and writes the data to a file

--- kinetics/time_dependent/tests/test_propagation_parallel.m
+++ kinetics/time_dependent/tests/test_propagation_parallel.m
@@ -3,12 +3,15 @@ function test_propagation_parallel()
   m_per_a0 = get_m_per_a0();
   ref_pressure_per_m3 = 6.44e24;
   
-  o3_molecules = {'666'};
+%   o3_molecules = {'666', '686'};
+  o3_molecules = {'686'};
   Js = [0:32, 36:4:64];
+%   Js = 24:25;
   Ks = 0:20;
   vib_syms_well = 0:1;
   temp_k = 298;
-  M_concs_per_m3 = 6.44 * logspace(23, 28, 6);
+%   M_concs_per_m3 = 6.44 * logspace(23, 28, 6);
+  M_concs_per_m3 = 6.44e24;
   dE_j = [-43.13, nan] * j_per_cm;
   dE_j(2) = get_dE_up(dE_j(1), temp_k);
   sigma0_tran_m2 = 1500 * m_per_a0^2;
@@ -25,13 +28,24 @@ function test_propagation_parallel()
 
   data_prefix = [fullfile('data', 'resonances'), filesep];
   krecs_m6_per_s = zeros(length(M_concs_per_m3), length(o3_molecules), length(Ks), length(Js), length(vib_syms_well));
-  krecs_vector = zeros(numel(krecs_m6_per_s), 1);
+  propagation_times = ...
+    zeros(length(M_concs_per_m3), length(o3_molecules), length(Ks), length(Js), length(vib_syms_well));
+  execution_times = zeros(length(M_concs_per_m3), length(o3_molecules), length(Ks), length(Js), length(vib_syms_well));
+  data_queue = parallel.pool.DataQueue;
+  data_queue.afterEach(@data_handler);
+
+  parpool(64);
   tic
-  parfor data_ind = 1:length(krecs_vector)
+  parfor data_ind = 1:numel(krecs_m6_per_s)
+    current_task = getCurrentTask();
     [M_ind, o3_ind, K_ind, J_ind, sym_ind] = ind2sub(size(krecs_m6_per_s), data_ind);
     [M_per_m3, o3_molecule, K, J, vib_sym_well] = ...
       deal(M_concs_per_m3(M_ind), o3_molecules{o3_ind}, Ks(K_ind), Js(J_ind), vib_syms_well(sym_ind));
+    disp(current_task.ID + ": pressure=" + M_per_m3 + ", molecule=" + o3_molecule + ", K=" + K + ", J=" + J + ...
+      ", sym=" + vib_sym_well + " started at " + string(datetime));
+    
     if K > J || J > 32 && mod(K, 2) == 1
+      disp(current_task.ID + ": Skipping J=" + J + ", K=" + K);
       continue
     end
 
@@ -57,19 +71,29 @@ function test_propagation_parallel()
   
     pressure_ratio = M_per_m3 / ref_pressure_per_m3;
     time_s = linspace(0, 100e-9, 51) / pressure_ratio;
+    tic
     [concentrations_per_m3, derivatives_per_m3_s, equilibrium_constants_m3] = propagate_concentrations_regions(...
       o3_molecule, states, initial_concentrations_per_m3, time_s, sigma0_tran_m2, temp_k, M_per_m3, dE_j, ...
       transition_models, region_names, K_dependent_threshold=K_dependent_threshold, ...
       separate_propagation=separate_propagation);
+    execution_time = toc;
   
     channel_ind = get_lower_channel_ind(o3_molecule);
     region_ind = 1;
-    next_krec_m6_per_s = get_krec(concentrations_per_m3(:, :, region_ind), derivatives_per_m3_s(:, :, region_ind), ...
-      equilibrium_constants_m3(:, :, region_ind), M_per_m3, channel_ind);
-    krecs_vector(data_ind) = next_krec_m6_per_s(end);
+    next_krec_m6_per_s = get_krec(concentrations_per_m3(end, :, region_ind), ...
+      derivatives_per_m3_s(end, :, region_ind), equilibrium_constants_m3(:, :, region_ind), M_per_m3, channel_ind);
+    propagation_time_s = time_s(size(concentrations_per_m3, 1) - 1);
+    send(data_queue, [M_ind, o3_ind, K_ind, J_ind, sym_ind, next_krec_m6_per_s, propagation_time_s, execution_time]);
+    disp(current_task.ID + ": pressure=" + M_per_m3 + ", molecule=" + o3_molecule + ", K=" + K + ", J=" + J + ...
+      ", sym=" + vib_sym_well + " finished propagation at " + string(datetime) + " after " + ...
+      execution_time + " seconds");
   end
   toc
-  
-  krecs_m6_per_s = reshape(krecs_vector, size(krecs_m6_per_s));
-  save("krecs.mat", "krecs_m6_per_s");
+
+  function data_handler(data)
+    krecs_m6_per_s(data(1), data(2), data(3), data(4), data(5)) = data(6);
+    propagation_times(data(1), data(2), data(3), data(4), data(5)) = data(7);
+    execution_times(data(1), data(2), data(3), data(4), data(5)) = data(8);
+    save("krecs.mat", "krecs_m6_per_s", "propagation_times", "execution_times");
+  end
 end
