3e619abb79be794b4ee1ab1c9c80c006d2914cf2
IgorGayday <gaidai92@gmail.com>
2022-10-10 10:25:40 -0500

    Adds a function to sum krec matrix with dtau multipliers

---
+++ kinetics/general/sum_krec_dtau.m
@@ -0,0 +1,13 @@
+function krec_total = sum_krec_dtau(krec_matrix, Ks, Js, vib_syms_well)
+% Adds up the values of krec with their respective dtau multipliers
+% krec_matrix is assumed to be K x J x sym. Ks, Js, and vib_sym_wells are 1D arrays that provide corresponding labels
+  krec_total = 0;
+  for J_ind = 1:size(krec_matrix, 2)
+    for K_ind = 1:size(krec_matrix, 1)
+      for sym_ind = 1:size(krec_matrix, 3)
+        dtau = get_dtau_2(Js(J_ind), Ks(K_ind), vib_syms_well(sym_ind));
+        krec_total = krec_total + krec_matrix(K_ind, J_ind, sym_ind) * dtau;
+      end
+    end
+  end
+end
