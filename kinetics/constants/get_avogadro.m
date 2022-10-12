f3e3cd1f30f8b0813fae2c1d0b6fc37ecb4e8018
IgorGayday <gaidai92@gmail.com>
2022-10-12 12:53:54 -0500

    Factors out constants from workspace to functions

---
+++ kinetics/constants/get_avogadro.m
@@ -0,0 +1,4 @@
+function num_per_mol = get_avogadro()
+% Returns number of particles per mol
+  num_per_mol = 6.02214076e+23;
+end
---
+++ kinetics/constants/get_gas_constant.m
@@ -0,0 +1,4 @@
+function R_j_per_mol_k = get_gas_constant()
+% Returns gas constant in J/(mol*K)
+  R_j_per_mol_k = 8.31446261815324;
+end
