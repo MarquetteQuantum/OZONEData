b5da15ba5b7454a8c58fed5c7ef9bd1c074a616e
IgorGayday <gaidai92@gmail.com>
2022-10-24 18:22:57 -0500

    Tightens convergence; Replaces min_time_steps with converged_steps

--- kinetics/time_dependent/src/propagation/get_ode_event_handler.m
+++ kinetics/time_dependent/src/propagation/get_ode_event_handler.m
@@ -9,12 +9,13 @@ function handle = get_ode_event_handler(derivatives_func, region_probs, equilibr
     channel_ind
     eval_step_s
     optional.comparison_factor = 2
-    optional.convergence = 0.01
-    optional.min_time_steps = 2
+    optional.convergence = 0.005
+    optional.converged_steps = 2
   end
 
   eval_times_s = 0;
-  krecs_m6_per_s = 0;
+  krecs_m6_per_s = inf;
+  converged_steps = 0;
   handle = @ode_event_handler;
 
   function [value, isterminal, direction] = ode_event_handler(time_s, concentrations_per_m3)
@@ -38,11 +39,14 @@ function handle = get_ode_event_handler(derivatives_func, region_probs, equilibr
       
       eval_times_s(end + 1) = time_s;
       krecs_m6_per_s(end + 1) = krec_m6_per_s;
-      if length(eval_times_s) > optional.min_time_steps
-        comparison_ind = find(time_s / optional.comparison_factor < eval_times_s, 1) - 1;
-        if abs(krecs_m6_per_s(end) / krecs_m6_per_s(comparison_ind) - 1) < optional.convergence
+      comparison_ind = find(time_s / optional.comparison_factor < eval_times_s, 1) - 1;
+      if abs(krecs_m6_per_s(end) / krecs_m6_per_s(comparison_ind) - 1) < optional.convergence
+        converged_steps = converged_steps + 1;
+        if converged_steps >= optional.converged_steps
           value = 0;
         end
+      else
+        converged_steps = 0;
       end
     end
   end
