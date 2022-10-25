73eedb79fbcf531b5d1ea0411afb1b26817badf9
IgorGayday <gaidai92@gmail.com>
2022-10-25 18:50:07 -0500

    Adds option to localize states

--- kinetics/time_dependent/src/process_states.m
+++ kinetics/time_dependent/src/process_states.m
@@ -6,6 +6,7 @@ function [states, ref_energy_j] = process_states(o3_molecule, states, energy_ran
     energy_range
     gamma_range
     optional.closed_channel = ""
+    optional.localize_states = false
   end
   states = assign_extra_properties(o3_molecule, states);
   [ref_energy_j, threshold_energy_j] = ...
@@ -19,5 +20,14 @@ function [states, ref_energy_j] = process_states(o3_molecule, states, energy_ran
   if optional.closed_channel ~= ""
     states{:, "gamma_" + optional.closed_channel} = 0;
     states{:, "gamma_total"} = sum(states{:, ["gamma_a", "gamma_b"]}, 2);
+  end
+
+  if optional.localize_states
+    sym_states = states{:, "sym"} > states{:, "asym"};
+    states{sym_states, "sym"} = sum(states{sym_states, ["sym", "asym"]}, 2);
+    states{sym_states, "asym"} = 0;
+    asym_states = ~sym_states;
+    states{asym_states, "asym"} = sum(states{asym_states, ["sym", "asym"]}, 2);
+    states{asym_states, "asym"} = 0;
   end
 end
