36b8286df7b67541cc99f9234b6958d4c62c7d8a
IgorGayday <gaidai92@gmail.com>
2022-11-03 15:46:54 -0500

    column and row functions support maps that change matrix size

--- general/column_function.m
+++ general/column_function.m
@@ -1,4 +1,13 @@
 function out = column_function(func, matrix)
   % applies a given function to each column of a given matrix
-  out = row_function(func, matrix.')';
+  if isempty(matrix)
+    return
+  end
+
+  col1_map = func(matrix(:, 1));
+  out = zeros(length(col1_map), size(matrix, 2));
+  out(:, 1) = col1_map;
+  for i = 2:size(matrix, 2)
+    out(:, i) = func(matrix(:, i));
+  end
 end
--- general/row_function.m
+++ general/row_function.m
@@ -1,15 +1,4 @@
 function out = row_function(func, matrix)
   % applies a given function to each row of a given matrix
-  if isempty(matrix)
-    return
-  end
-
-  out = cell(size(matrix, 1), 1);
-  for i = 1:size(matrix, 1)
-    out{i} = func(matrix(i, :));
-  end
-
-  if isnumeric(out{1}) || islogical(out{1})
-    out = cell2mat(out);
-  end
+  out = column_function(func, matrix')';
 end
