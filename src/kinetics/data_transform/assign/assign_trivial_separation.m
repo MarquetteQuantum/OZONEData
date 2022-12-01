6ab50392db2da5f7d9ec738db79f8018ce5806a5
IgorGayday <gaidai92@gmail.com>
2022-12-01 16:12:40 -0600

    Adds vdw_a region

--- src/kinetics/data_transform/assign/assign_trivial_separation.m
+++ src/kinetics/data_transform/assign/assign_trivial_separation.m
@@ -2,6 +2,7 @@ function states = assign_trivial_separation(states)
 % Add trivial separation to monoisotopic species to make processing uniform with isotopically substituted cases
   states{:, "sym"} = states{:, "cov"};
   states{:, "asym"} = 0;
+  states{:, "vdw_a"} = states{:, "vdw"};
   states{:, "vdw_a_sym"} = states{:, "vdw"};
   states{:, "vdw_a_asym"} = 0;
   states{:, "vdw_b"} = 0;
--- src/kinetics/data_transform/assign/assign_vdw.m
+++ src/kinetics/data_transform/assign/assign_vdw.m
@@ -1,4 +1,5 @@
 function states = assign_vdw(states)
 % Assigns total probability in Van der Waals regions
+  states{:, "vdw_a"} = states{:, "vdw_a_sym"} + states{:, "vdw_a_asym"};
   states{:, "vdw"} = states{:, "vdw_a_sym"} + states{:, "vdw_a_asym"} + states{:, "vdw_b"};
 end
