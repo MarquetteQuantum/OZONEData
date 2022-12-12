1e1279b8ef2aee0c63038105e8d30692e27fcecc
IgorGayday <gaidai92@gmail.com>
2022-12-12 15:16:29 -0600

    Adds an option to specify alpha0 separately for each type of cross-region transition

--- src/kinetics/general/calculate_transition_matrix_unitless_separate.m
+++ src/kinetics/general/calculate_transition_matrix_unitless_separate.m
@@ -1,7 +1,9 @@
 function matrix = calculate_transition_matrix_unitless_separate(states, dE_j, region_names, alpha0, optional)
 % Calculates unitless state-to-state transition matrix (matrix(i, j) = kappa i->j)
 % dE = [down, up]
-% alpha0 is from 0 to 1 and controls efficiency of cross-region transitions
+% alpha0 controls efficiency of cross-region transitions
+% alpha0 can be a matrix of alpha values for each type of cross-transition or a scalar, which is interpreted as a constant matrix 
+% Transitions within regions are unaffected by alpha0 regardless of the values of the diagonal entries
   arguments
     states
     dE_j
@@ -10,6 +12,9 @@ function matrix = calculate_transition_matrix_unitless_separate(states, dE_j, re
     optional.region_factors = ones(size(region_names))
   end
 
+  if length(alpha0) == 1
+    alpha0 = alpha0 * ones(length(region_names));
+  end
   block_size = [size(states, 1), length(region_names)];
   matrix = zeros(prod(block_size));
   energies_j = states{:, "energy"};
@@ -24,7 +29,7 @@ function matrix = calculate_transition_matrix_unitless_separate(states, dE_j, re
       if region_row == region_col
         ptran = ptran / optional.region_factors(region_row);
       else
-        alpha = 3 * alpha0 * ...
+        alpha = 3 * alpha0(region_row, region_col) * ...
           (probs(state_row, region_row) * probs(state_col, region_row) / optional.region_factors(region_row) + ...
           probs(state_row, region_col) * probs(state_col, region_col) / optional.region_factors(region_col));
         ptran = ptran * alpha / optional.region_factors(region_row) / optional.region_factors(region_col);
